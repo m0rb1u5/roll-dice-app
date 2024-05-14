a67a807dcab1bb9a3e8273d3b94415ee65a70d6b
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2024-05-14 12:56:10 -0300

    38. Practice: Create a Custom Widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -15,10 +15,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(color: Colors.white, fontSize: 28),
-        ),
+        child: StyledText(),
       ),
     );
   }
