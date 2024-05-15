97ded89f7d5fb85f06640f310814374d4aedc6cb
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2024-05-15 11:26:14 -0300

    39. Introducing Variables

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -4,14 +4,17 @@ import 'package:roll_dice_app/styled_text.dart';
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
+  static const startAlignment = Alignment.topLeft;
+  static const endAlignment = Alignment.bottomRight;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
         gradient: LinearGradient(
           colors: [Colors.blue, Colors.purple],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
